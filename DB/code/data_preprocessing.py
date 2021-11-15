c98ba434b5f95c995aba49769ef7e72eafc4cddf
KyungHyun Jovinus Lee <lkh256@gmail.com>
2021-11-15 15:06:51 +0900

    fix bugs

--- DB/code/data_preprocessing.py
+++ DB/code/data_preprocessing.py
@@ -4,14 +4,20 @@ import numpy as np
 import re
 pd.set_option("display.max_columns", None)
 # %%
-df_cur_db = pd.read_excel("../data/asthma_dataset_final.xlsx", sheet_name='746 (소아2 중복3 missing 8제거)')
+df_cur_ai_db = pd.read_excel("../data/asthma_dataset_final.xlsx", sheet_name='746 (소아2 중복3 missing 8제거)')
+
+df_cur_cut_db = pd.read_excel('../data/20210913 ISE-FENO eos.xlsx', sheet_name='669 (77 missing eos제외)')
 # %%
 df_new_db = pd.read_excel("../data/validation_set_parsed.xlsx")
 
 # %%
 subs = r"0.05|0.5|2.0|8.0|16.0|32.0"
-mbpt_txt = [x for x in df_cur_db.columns if re.search(subs, str(x))]
-baseline_feature = ['PtID', 'IndexDate', 'AgeAtDx', 'Sex_f1m2', 'BMI', 'Smk', 'Sx_dyspnea', 'Sx_cough', 'Sx_wheezing', 'Co_rhinitis', 'Skintest', 'IgE', 'Sx_cough', 'Sx_wheezing', 'Asthma', 'MBPT_result', 'PC20_16', 'FeNO', 'ISE_Eo3%', 'ISE_Eos', 'ISE_Neu']
+mbpt_txt = [x for x in df_cur_ai_db.columns if re.search(subs, str(x))]
+baseline_feature = ['PtID', 'IndexDate', 'AgeAtDx', 'Sex_f1m2', 'BMI', 
+                    'Smk', 'Sx_dyspnea', 'Sx_cough', 'Sx_wheezing', 
+                    'Co_rhinitis', 'Skintest', 'IgE', 'Sx_cough', 
+                    'Sx_wheezing', 'Asthma', 'MBPT_result', 'PC20_16', 
+                    'FeNO', 'ISE_Eo3%', 'ISE_Eos', 'ISE_Neu', 'Lab_EosCount', 'Lab_Eos(%)']
 # %%
 df_new_db = df_new_db[baseline_feature + mbpt_txt]
 df_new_db = df_new_db.assign(Sex_f1m2=lambda x: x['Sex_f1m2'].map({'F':1, 'M':2}), 
@@ -20,11 +26,17 @@ df_new_db = df_new_db.assign(Sex_f1m2=lambda x: x['Sex_f1m2'].map({'F':1, 'M':2}
                              )
 df_new_db = df_new_db.query('ISE_Eos.notnull()', engine='python')
 # %%
-df_cur_db = df_cur_db[baseline_feature + mbpt_txt]
-df_cur_db = df_cur_db.assign(Skintest=lambda x: x['Skintest'].map({'P':2, 'B':1, 'N':0, 'N(Dermo)':0}))
+df_cur_ai_db = df_cur_ai_db[baseline_feature + mbpt_txt]
+df_cur_ai_db = df_cur_ai_db.assign(Skintest=lambda x: x['Skintest'].map({'P':2, 'B':1, 'N':0, 'N(Dermo)':0}))
+
+df_cur_cut_db = df_cur_cut_db[baseline_feature + mbpt_txt]
 # %%
-df_db = pd.concat((df_cur_db, df_new_db), axis=0)
+df_ai_db = pd.concat((df_cur_ai_db, df_new_db), axis=0)
+
+df_cut_db = pd.concat((df_cur_cut_db, df_new_db), axis=0)
+
 # %%
-df_db.to_csv('../data/asthma_ai_dataset.csv', index=False, encoding='utf-8')
+df_ai_db.to_csv('../data/asthma_ai_dataset.csv', index=False, encoding='utf-8')
+df_cut_db.to_csv('../data/ISE_3_cut_dataset.csv', index=False, encoding='utf-8')
 
 # %%
