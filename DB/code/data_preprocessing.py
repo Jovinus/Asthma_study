d9163a1808525e184095ab0631e603b0d4124a06
KyungHyun Jovinus Lee <lkh256@gmail.com>
2021-12-17 01:12:40 +0900

    add extra skin test results

--- DB/code/data_preprocessing.py
+++ DB/code/data_preprocessing.py
@@ -35,7 +35,15 @@ df_ai_db = pd.concat((df_cur_ai_db, df_new_db), axis=0)
 df_cut_db = pd.concat((df_cur_cut_db, df_new_db), axis=0)
 
 # %%
+df_skin = pd.read_csv("../data/Skin_Test/skin_test.csv")
+df_skin = df_skin.assign(IndexDate = lambda x: pd.to_datetime(x['처방일자#5']), 
+                         Skintest_new = lambda x: x['test_result'].astype(int)).drop(columns=['처방일자#5', 'test_result'])
+
+df_ai_db = pd.merge(df_ai_db, df_skin, on=['PtID', 'IndexDate'], how='left')
+df_ai_db = df_ai_db.assign(SkinTest = lambda x: np.where(x['Skintest_new'].notnull(), x['Skintest_new'], x['Skintest'].map({1:1, 2:1})))
+
+df_cut_db = pd.merge(df_cut_db, df_skin, on=['PtID', 'IndexDate'], how='left')
+df_cut_db = df_cut_db.assign(SkinTest = lambda x: np.where(x['Skintest_new'].notnull(), x['Skintest_new'], x['Skintest'].map({1:1, 2:1})))
+# %%
 df_ai_db.to_csv('../data/asthma_ai_dataset.csv', index=False, encoding='utf-8')
 df_cut_db.to_csv('../data/ISE_3_cut_dataset.csv', index=False, encoding='utf-8')
-
-# %%
