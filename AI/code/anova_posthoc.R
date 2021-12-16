9d47399190a98e9188bc7c54dc0673d9408d73d8
KyungHyun Lee <lkh256@gmail.com>
2021-12-17 01:21:09 +0900

    comit for merge

--- AI/code/anova_posthoc.R
+++ AI/code/anova_posthoc.R
@@ -1,12 +1,23 @@
-setwd("/home/lkh256/Studio/Asthma/AI/code/")
-
+library(stats)
+library(readxl)
+library(xlsx)
 library(data.table)
+library(tidyverse)
+library(dplyr)
 library(moonBook)
 
-df_orig <- fread("../result/selected_metric/selected_metric.csv")
-View(df_orig)
+setwd("/Users/kyunghyunlee/Studio/Asthma_study/AI/")
+metrics <- fread('./result/selected_metric/selected_metric.csv')
+View(metrics)
 
-results <- pairwise.t.test(x = df_orig$test_roc_auc, g = df_orig$feature, p.adjust.method = "bonferroni")
-View(results)
+results <- pairwise.t.test(x = metrics$test_roc_auc, g = metrics$feature, p.adjust.method = "bonferroni")
+results
 
-results$p.value
+output_table <- mytable(feature ~ test_auroc + test_auprc + test_accuracy + test_sensitivy + test_specificity , data=metrics, digits=2)
+output_table <- mytable(feature ~ . , data=metrics, digits=2)
+output_table <- summary(output_table)
+
+mycsv(x=output_table, file="./descriptive_metrics.csv")
+
+metrics <- fread('./descriptive_metrics.csv')
+write.xlsx(x=metrics, file="./descriptive_metrics.xlsx")
